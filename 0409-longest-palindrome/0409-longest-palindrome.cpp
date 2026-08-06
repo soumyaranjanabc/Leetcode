eaf6f481f954dd874e42118cd6888537e72fd61e
Soumya  Ranjan Mohanty <sureshmohanty141@gmail.com>
2026-08-06 09:07:22 +0530

    Time: 0 ms (100%), Space: 9.2 MB (68.72%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        vector<int> lower(26,0),upper(26,0);
+        for(int i=0;i<s.size();i++){
+            if(s[i]>='a'){
+                lower[s[i]-'a']++;
+            }
+            else{
+                upper[s[i]-'A']++;
+            }
+        }
+        int count =0;
+        bool odd=0;
+        for(int i=0;i<26;i++){
+            if(lower[i]%2==0){
+                count=count+lower[i];
+            }
+            else{
+                count = count+(lower[i]-1);
+                odd=1;
+            }
+            if(upper[i]%2==0){
+                count = count + upper[i];
+            }
+            else{
+                count = count+(upper[i]-1);
+                odd=1;
+            }
+        }
+
+        return count+odd;
+        
+        
+    }
+};
